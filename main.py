07a556929b437ffe7c68fdeca5d15bf9cb163646
Barrett <breinhard@csumb.edu>
2022-12-10 15:04:08 -0800

    Finished - image and video now display on page

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from flask import Flask, request, render_template
+# from jinja2 import Environment, FileSystemLoader
 import os
 import openai
 import urllib.request
 import re
 import credentials
 # from flask_bootstrap import Bootstrap5, Bootstrap
-# flask --app temp.py --debug run 
+# flask --app temp.py --debug run
 app = Flask(__name__)
 # boostrap = Bootstrap(app)
 
-
-
+root = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(root, 'html', 'project_template.html')
 
 @app.route('/')
 def home():
@@ -35,12 +36,12 @@ def image():
     for youtube_search in genre:
         html = urllib.request.urlopen(f"https://www.youtube.com/results?search_query={youtube_search}")
         video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-        final_url = "https://www.youtube.com/watch?v="
+        final_url = "https://www.youtube.com/embed/"
         youtube_search_list.append((f"{final_url}{video_ids[0]}"))
 
-    return "Your description was: \"" + image_url + "\" and your Genre was: \"" + str(youtube_search_list) + "\""
+    # return "Your description was: \"" + image_url + "\" and your Genre was: \"" + str(youtube_search_list) + "\""
 
-
+    return render_template('playlist.html', img_url = image_url, yt_url = youtube_search_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
