37bb15969808b8b3933b24e97f610d03ca449f09
Sebastian Vera <severa@csumb.edu>
2022-12-16 02:49:43 -0800

    final commit

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Sebastian Vera : worked on youtube search and open ai image search
+# Camille Clendenon : worked on flask website and implementing search bars
+# Barrett Reinhard : worked on css and rendering images and youtube videos on page
+
 from flask import Flask, request, render_template
 # from jinja2 import Environment, FileSystemLoader
 import os
@@ -19,24 +23,31 @@ def home():
 
 @app.route('/', methods = ['POST'])
 def image():
-    #open ai generation
+    #open ai generation - requires api key in credentials.py - we use gitignore to hide key - key in trello
     image_des = request.form.get("idescript").lower()
     #resource: https://www.youtube.com/watch?v=czx7CxzcJzQ
     response = openai.Image.create(  prompt = image_des, 
-    n=2,
+    n=1, #number of images to generate
     size="1024x1024"
     )
+    #retrieves url of image
     image_url = response['data'][0]['url']
 
     #youtube search
     youtube_search_list = []
+    #splits the searches by the comma
     genre = request.form.get("genre").split(", ")
-    #works! makes every element in list have a + if space
+    #makes every element in list have a + in place of a space
     genre = [string.replace(' ', '+') for string in genre]
     for youtube_search in genre:
         html = urllib.request.urlopen(f"https://www.youtube.com/results?search_query={youtube_search}")
+        #if kanye is searched for, skip over
+        if youtube_search.lower() == "kanye": 
+            continue
+        #uses regex. Each video has an 11 character sequence. This should match every 11 character sequence and store in list
         video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
         final_url = "https://www.youtube.com/embed/"
+        #takes the first 11 char sequence video id i.e. first result from search and appends this to the search list
         youtube_search_list.append((f"{final_url}{video_ids[0]}"))
 
     # return "Your description was: \"" + image_url + "\" and your Genre was: \"" + str(youtube_search_list) + "\""
